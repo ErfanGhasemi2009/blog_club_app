416bcb7f3e0b281fab66da21fae2c8160a7affb4
Erfan Ghasemi <erfanghasemiioogg@gmail.com>
2024-07-20 18:47:30 +0330

    add stories

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blog_club_app/data.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,31 +7,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const String fontFamilyDefualt = "Avenir";
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  const Color primaryTextColor = Color(0xff0D253C);
+  const Color secondryTextColor = Color(0xff2D4379);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: const TextTheme(
+          titleSmall: TextStyle(
+              fontFamily: fontFamilyDefualt,
+              color: secondryTextColor,
+              fontWeight: FontWeight.w200,
+              fontSize: 18),
+          headlineLarge: TextStyle(
+              color: primaryTextColor,
+              fontFamily: fontFamilyDefualt,
+              fontSize: 24,
+              fontWeight: FontWeight.bold),
+          bodySmall: TextStyle(
+              color: secondryTextColor,
+              fontFamily: fontFamilyDefualt,
+              fontSize: 12),
+        ),
       ),
       home: const HomeState(),
     );
@@ -41,10 +48,152 @@ class HomeState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final stories = AppDatabase.stories;
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonathan!',
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    Image.asset(
+                      "assets/img/icons/notification.png",
+                      width: 32,
+                      height: 32,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
+                child: Text(
+                  'Explore today’s',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+              _StoryList(stories: stories)
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class _StoryList extends StatelessWidget {
+  const _StoryList({
+    super.key,
+    required this.stories,
+  });
+
+  final List<StoryData> stories;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+    
+      child: ListView.builder(
+          itemCount: stories.length,
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+          itemBuilder: (context, index) {
+            final story = stories[index];
+            return _Story(story: story);
+          }),
+    );
+  }
+}
+
+class _Story extends StatelessWidget {
+  const _Story({
+    super.key,
+    required this.story,
+  });
+
+  final StoryData story;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(4, 2, 4, 0),
+      child: Column(
+        children: [
+          Stack(children: [
+            story.isViewed ? _profileImageVisited() : _profileImageNotVisited(),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ))
+          ]),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            story.name,
+            style: Theme.of(context).textTheme.bodySmall,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _profileImageNotVisited() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          gradient: const LinearGradient(begin: Alignment.topLeft, colors: [
+            Color(0xff376AED),
+            Color(0xff49B0E2),
+            Color(0xff9CECFB)
+          ])),
+      child: Container(
+        margin: EdgeInsets.all(2),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(22), color: Colors.white),
+        padding: EdgeInsets.all(5),
+        child: _profileImage(),
+      ),
+    );
+  }
+
+  Widget _profileImageVisited() {
+    return SizedBox(
+      width: 68,
+      height: 68,
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        strokeWidth: 2,
+        dashPattern: [8, 3],
+        color: const Color(0xff7B8BB2),
+        radius: const Radius.circular(24),
+        padding: const EdgeInsets.all(7),
+        child: Container(
+          child: _profileImage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImage() {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(17),
+        child: Image.asset('assets/img/stories/${story.imageFileName}'));
   }
 }
